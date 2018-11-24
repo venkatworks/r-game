0e5264058cbe9f59a445414aebe43e8d3c968935
venkatesh <venky.utility@gmail.com>
2018-11-24 17:19:36 +0530

    grid

--- r-game/src/app/rgame/rgame.component.ts
+++ r-game/src/app/rgame/rgame.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RgameComponent implements OnInit {
 
+  cells = [];
+  robos = [".","1",".","1",".",".","2",".",".",".","3",".",".","3",".",".",".","1",".","1","."]
+
   constructor() { }
 
   ngOnInit() {
+    for(let i=0;i<this.robos.length;i++){
+      this.cells.push({
+        cellIndex:i,
+        robo:this.robos[i],
+        status:false
+      })
+    }
+  }
+
+  
+
+  onCellClick(cell) {
+     // check is the move will conflict or not.
+     console.log(cell);
+     // current cell value
+     // need to check with all the other cell
+    
   }
 
 }
